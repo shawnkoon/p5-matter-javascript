d5916fc8a9d055e33f7fc9e7984b471f6795a24e
shawnkoon <shawn9405@gmail.com>
2017-07-01 12:20:23 -0700

    p5 adapted to matter to draw with angle + added styling + static boundary

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var boxes = [];
+
+var ground;
+
 function setup() {
   createCanvas(400, 400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  var options = {
+    isStatic: true,
+  };
+
+  ground = Bodies.rectangle(200, height, width, 10, options);
+  World.add(world, ground);
 }
 
 function draw() {
   background(51);
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].show();
+  }
+  fill(255);
+  strokeWeight(4);
+  line(0, height, width, height);
+}
+
+function mouseDragged() {
+  boxes.push(new Box(mouseX, mouseY, 20, 20));
+
 }
